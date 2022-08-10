c3977cbe65e7bb365d60b1def8859478bbde114f
filip.prusik <prusikfilip@gmail.com>
2022-08-10 15:27:41 +0200

    First steps with communication

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 import random
 import math
+import server
+from threading import Thread
 
 pygame.init()
 dis_width = 1000
@@ -8,8 +10,8 @@ dis_height = 800
 dis = pygame.display.set_mode((dis_width, dis_height), pygame.DOUBLEBUF)
 
 pygame.display.set_caption('Achtung die Kurve')
-main_icon = pygame.image.load('head.jpg')
-pygame.display.set_icon(main_icon)
+# main_icon = pygame.image.load('head.jpg')
+# pygame.display.set_icon(main_icon)
 
 game_over = False
 blue = (54, 180, 240, 255)
@@ -17,31 +19,29 @@ cgrey = (59, 68, 72, 255)
 cred = (222, 14, 68, 255)
 rad = 1
 players = []
-# player_move = 2
-# player_speed = 60
-# playerx = int(dis_width/2)
-# playery = int(dis_height/2)
-# prev_playerx = 0
-# prev_playery = 0
-# alpha = math.pi/4
-# beta = math.pi/4
-# x_change = random.randint(0, 6) - 3
-# y_change = random.randint(0, 6) - 3
-# # x_change = math.pi/random.randint(0, 180)
-# # y_change = math.pi/random.randint(0, 180)
-# player_line = []
-# player_line.append([playerx, playery])
-# prev_x = None
-# prev_y = None
 
 clock = pygame.time.Clock()
 dis.fill(cgrey)
+dis.blit(pygame.font.SysFont("bahnschrift", 25).render(' Click S to start', True, cred), [dis_width/2, dis_height/2])
 pygame.display.update()
+stop_threads = False
+
+
+# Splash screen
+while True: 
+    for event in pygame.event.get():
+        if event.type==pygame.QUIT:
+            pygame.quit()
+            quit()
+    key_input = pygame.key.get_pressed()
+    #print(key_input)
+    if key_input[pygame.K_s]:
+        break
 
 class Player():
     def __init__(self, color, name):
         self.player_move = 2
-        self.player_speed = 60
+        self.player_speed = 20 #original: 60
         self.playerx = int(random.randint(dis_width/10, dis_width*0.9))
         self.playery = int(random.randint(dis_height/10, dis_height*0.9))
         self.prev_playerx = 0
@@ -73,6 +73,7 @@ def newgame():
     player2 = Player(cred, 'Damian')
     players.append(player1)
     players.append(player2)
+    #t = Thread(target=server.listeningGuest, args=(players[0].alpha))
     dis.fill(cgrey)
     pygame.display.update()
     gameLoop()
@@ -91,15 +92,24 @@ def nextmove(player):
                 quitgame(player)
 
 def quitgame(loser):
+    #global stop_threads
     print(str(loser) + ' lose the game')
     game_over = True
     dis.fill(cgrey)
     dis.blit(pygame.font.SysFont("bahnschrift", 25).render(str(loser) + ' lose the game', True, cred), [dis_width/6, dis_height/6])
     pygame.display.update()
+    server.stopThread()
+    t.join()
     while game_over:
         for event in pygame.event.get():
                 if event.type==pygame.KEYDOWN:
                     if event.key == pygame.K_q:
+                        print('Try to excape')
+                        #t.stop()
+                        # server.stopThread()
+                        #stop_threads = True
+                        # t.join()
+                        #print('After join()')
                         pygame.quit()
                         quit()
                     if event.key == pygame.K_c:
@@ -137,6 +147,9 @@ def paint(gamer):
 
 def gameLoop():
     global game_over
+    
+    t.start()
+    print('STARTS')
     while not game_over:
         for event in pygame.event.get():
             if event.type==pygame.QUIT:
@@ -150,72 +163,27 @@ def gameLoop():
             players[1].alpha += math.pi/45
         if key_input[pygame.K_s]:
             players[1].alpha -= math.pi/45
-        
+        if server.moveL == True:
+            players[1].alpha += math.pi/45
+            server.moveL = False
+            print('move L made')
+        if server.moveR == True:
+            players[1].alpha -= math.pi/45
+            server.moveR = False
+            print('move R made')
         nextmove(players[0])
         nextmove(players[1])
-        # prev_playerx = playerx
-        # prev_playery = playery
-
-        """ WORKING
-        player1.playerx += round(math.sin(player1.alpha) * player1.player_move, 1)
-        player1.playery += round(math.cos(player1.alpha) * player1.player_move, 1)
-        player1.playerx = round(player1.playerx, 1)
-        player1.playery = round(player1.playery, 1)
-        for i in range(len(player1.player_line)):
-            if abs(player1.player_line[i][0]-player1.playerx)<1 and abs(player1.player_line[i][1]-player1.playery)<1:
-                count += 1
-                print(str(count) + ' Catched')
-
-
-        player2.playerx += round(math.sin(player2.alpha) * player2.player_move, 1)
-        player2.playery += round(math.cos(player2.alpha) * player2.player_move, 1)
-        player2.playerx = round(player2.playerx, 1)
-        player2.playery = round(player2.playery, 1)
-        for i in range(len(player2.player_line)):
-            if abs(player2.player_line[i][0]-player2.playerx)<1 and abs(player2.player_line[i][1]-player2.playery)<1:
-                count += 1
-                print(str(count) + ' Catched')"""
-
-        # prev_playerx = playerx
-        # prev_playery = playery
-        # playerx += x_change
-        # playery += y_change
-
-
-        # x_change = math.tan(beta)*y_change
-        # y_change = math.tan(alpha)*x_change
-        # playerx += x_change
-        # playery += y_change
-
-        # print('prev: ' + str(prev_playerx))
-        # print(playerx)
-        #print(str(playerx) + ' ' + str(playery))
-        #print(dis.get_at((playerx, playery)))
-        # if dis.get_at((round(playerx), round(playery))) == blue:
-        #     print(str(count) + ' Game over')
-        #     count += 1
-        #     quitgame()
-        # else:
-        #     player_line.append([playerx, playery])
-        #dis.fill(cgrey)
+        
         paint(players[0])
         paint(players[1])
-        # for i in range(len(player_line)):
-        #     pygame.draw.circle(dis, blue, [player_line[i][0], player_line[i][1]], rad)
-        #pygame.draw.circle(dis, blue, [playerx, playery], rad)
-        #pygame.draw.line(dis, blue, [dis_width, dis_height],[playerx, playery])
-        #pygame.draw.circle(dis, blue, [50, 50], rad)
-        #mesg = pygame.draw.circle(dis, blue, [50, 50], rad)
-        #dis.blit(dis, [dis_width/6, dis_height/6])
-        #pygame.display.update()
+       
         pygame.display.update()
 
         clock.tick(players[0].player_speed)
 
-# player1 = Player(blue, 'Filip')
-# player2 = Player(cred, 'Damian')
-# players = [player1, player2]
 
+#t = Thread(target=just4test)
+t = Thread(target=server.listeningGuest)
 newgame()
 #gameLoop()
 quitgame()
--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 import socket
+from math import pi
 
 # HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
 HOST = "192.168.8.101"  # Standard loopback interface address (localhost)
@@ -6,24 +7,44 @@ HOST = "192.168.8.101"  # Standard loopback interface address (localhost)
 print(HOST)
 # PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
 PORT = 12345  # Port to listen on (non-privileged ports are > 1023)
+go_thread = True
+moveL = False
+moveR = False
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    #print(s.listen())
-    
-    while True:
-        conn, addr = s.accept()
-        with conn:
-            print(f"Connected by {addr}")
-            #while True:
-            data = conn.recv(1024)
-            print(data)
-            data = int.from_bytes(data, 'big')
-            # print()
-            # data = data + str.encode(' filip ')
-            print(data)
-            # if not data:
-            #     break
-            conn.sendall(b'Hello, raspberry')
-            
+def listeningGuest():
+    print('in listeningGuest')
+    global moveL, moveR
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        print('socket')
+        s.bind((HOST, PORT))
+        print('after bind')
+        s.listen()
+        print('after listen')
+        s.settimeout(2.0) # is added to avoid situation when while loop is waiting for response from client. Loop will be resetting every 2.0 sec
+        
+        while go_thread:
+            print('in the loop')
+            try:
+                conn, addr = s.accept()
+                print('after accept')
+                with conn:
+                    print(f"Connected by {addr}")
+                    #while True:
+                    data = conn.recv(8)
+                    print(data)
+                    data = int.from_bytes(data, 'big')
+                    # print()
+                    # data = data + str.encode(' filip ')
+                    print(data)
+                    if data == 1: moveL = True
+                    if data == 2: moveR = True
+                    # if not data:
+                    #     break
+                    #conn.sendall(b'Hello, raspberry')
+            except:
+                print('TIMEOUT')
+        print('Thread terminates')
+def stopThread():
+    global go_thread
+    go_thread = False
+    print('in function')
